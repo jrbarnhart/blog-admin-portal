841b364050f446b37b52d9a3c90345aab7a8cc23
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-14 16:47:42 -0500

    Create context for login token

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,14 +1,21 @@
+"use client";
+
+import { createContext, useContext, useState } from "react";
 import SideNav from "../ui/sidenav";
+
+const TokenContext = createContext({});
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [token, setToken] = useState({ text: "", isAdmin: false });
+
   return (
-    <>
+    <TokenContext.Provider value={{ token, setToken }}>
       <SideNav />
       {children}
-    </>
+    </TokenContext.Provider>
   );
 }
