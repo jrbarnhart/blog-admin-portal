8e699f28cef1ae54cb23a10661b7469603cb3239
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-14 13:19:15 -0500

    Add basic fn for fetching posts

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,10 +1,13 @@
 import { UserLogin, Post, Comment } from "@/app/lib/definitions";
 
+const api = process.env.BACKEND_API_URL;
+
 // Log in
 export async function logIn(userLogin: UserLogin) {
   // Send request to API then await and return response
   try {
-    const jwtoken = await fetch(`${process.env.BACKEND_API_URL}/users/login`, {
+    const jwtoken = await fetch(`${api}/users/login`, {
+      method: "POST",
       body: JSON.stringify(userLogin),
       cache: "no-store",
     });
@@ -16,6 +19,15 @@ export async function logIn(userLogin: UserLogin) {
 }
 
 // Get all the posts data
+export async function getPosts() {
+  try {
+    const posts = await fetch(`${api}/posts`);
+    return posts;
+  } catch (error) {
+    console.error("Database Error", error);
+    throw new Error("Failed to fetch posts.");
+  }
+}
 
 // Create a new post
 
