d0a076731b68ac81891dfd3bac5b2534537fe7c1
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-14 14:07:19 -0500

    Map links to Link components

--- app/ui/nav-links.tsx
+++ app/ui/nav-links.tsx
@@ -6,3 +6,17 @@ const links = [
   { name: "Log In", href: "/login" },
   { name: "Create Post", href: "/create-post" },
 ];
+
+export default function NavLinks() {
+  return (
+    <>
+      {links.map((link) => {
+        return (
+          <Link key={link.name} href={link.href}>
+            {link.name}
+          </Link>
+        );
+      })}
+    </>
+  );
+}
