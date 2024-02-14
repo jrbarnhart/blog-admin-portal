bf5886d737a2d3ebb2dba1463ef3168886163a31
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-14 13:10:00 -0500

    Add fn for logging in that returns jwt

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,8 +1,18 @@
 import { UserLogin, Post, Comment } from "@/app/lib/definitions";
 
 // Log in
-export function logIn(userLogin: UserLogin) {
+export async function logIn(userLogin: UserLogin) {
   // Send request to API then await and return response
+  try {
+    const jwtoken = await fetch(`${process.env.BACKEND_API_URL}/users/login`, {
+      body: JSON.stringify(userLogin),
+      cache: "no-store",
+    });
+    return jwtoken;
+  } catch (error) {
+    console.error("Database Error", error);
+    throw new Error("Failed to login.");
+  }
 }
 
 // Get all the posts data
