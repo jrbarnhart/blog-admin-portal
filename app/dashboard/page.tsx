0b69e4c263b99a9707ad295af221385f72c20a42
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-14 16:06:56 -0500

    Add placeholder page content

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+export default function Dashboard() {
+  return (
+    <main>
+      <p>This is some dashboard page content!</p>
+      <p>Total Posts: Many</p>
+      <p>Total Comments: A Lot</p>
+      <p>Published: All / Unpublished: 0</p>
+    </main>
+  );
+}
