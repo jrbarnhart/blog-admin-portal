bb0e447b199ab1da3aace62fae9b8aed22ac7a77
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-14 15:13:35 -0500

    Replace login with home link

--- app/ui/nav-links.tsx
+++ app/ui/nav-links.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 
 const links = [
+  { name: "Home", href: "/" },
   { name: "Posts", href: "/posts" },
-  { name: "Log In", href: "/login" },
   { name: "Create Post", href: "/create-post" },
 ];
 
