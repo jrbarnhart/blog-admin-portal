165c3ee20641f6934f8f1cab3c5d3676beed4fb1
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-14 14:46:48 -0500

    Remove home link

--- app/ui/nav-links.tsx
+++ app/ui/nav-links.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 
 const links = [
-  { name: "Home", href: "/" },
   { name: "Posts", href: "/posts" },
   { name: "Log In", href: "/login" },
   { name: "Create Post", href: "/create-post" },
