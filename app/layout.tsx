aa087f73523c19c688a0ec162b613b7d1697ec60
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-14 16:01:22 -0500

    Remove sidenav

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import SideNav from "./ui/sidenav";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +19,6 @@ export default function RootLayout({
       <body
         className={`${inter.className} flex h-screen flex-col md:flex-row md:overflow-hidden`}
       >
-        <SideNav />
         {children}
       </body>
     </html>
