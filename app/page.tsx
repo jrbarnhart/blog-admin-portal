862020e237d6ebfcf9575733c157be1f05ed0bbd
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-14 15:59:44 -0500

    Add link to dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <p>This is some home page content!</p>
-      <p>Total Posts: Many</p>
-      <p>Total Comments: A Lot</p>
-      <p>Published: All / Unpublished: 0</p>
+      <Link href="/dashboard">Continue to Dashboard</Link>
     </main>
   );
 }
