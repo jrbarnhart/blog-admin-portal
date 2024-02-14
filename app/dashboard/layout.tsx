3e949034c92c4c96e3c1b822011c03a950506bb3
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-14 16:59:35 -0500

    Remove context

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,21 +1,14 @@
-"use client";
-
-import { createContext, useContext, useState } from "react";
 import SideNav from "../ui/sidenav";
-
-const TokenContext = createContext({});
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [token, setToken] = useState({ text: "", isAdmin: false });
-
   return (
-    <TokenContext.Provider value={{ token, setToken }}>
+    <>
       <SideNav />
       {children}
-    </TokenContext.Provider>
+    </>
   );
 }
