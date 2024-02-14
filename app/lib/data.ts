7a4199ceaa9cbdbe74d210affc1df1802698379d
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-14 12:42:19 -0500

    Add placeholder fn for login

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,4 +1,9 @@
+import { UserLogin, Post, Comment } from "@/app/lib/definitions";
+
 // Log in
+export function logIn(userLogin: UserLogin) {
+  // Send request to API then await and return response
+}
 
 // Get all the posts data
 
