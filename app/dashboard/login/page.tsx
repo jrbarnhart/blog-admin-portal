efe1e799979d30f308c190479ff4cfb2e66bd740
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-15 15:58:33 -0500

    Add login form component

--- app/dashboard/login/page.tsx
+++ app/dashboard/login/page.tsx
@@ -1,8 +1,10 @@
+import LoginForm from "@/app/ui/login-form";
+
 export default function Login() {
   return (
     <>
       <h2>Log In</h2>
-      {/* <LoginForm /> */}
+      <LoginForm />
     </>
   );
 }
