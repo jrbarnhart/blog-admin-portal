f74a9f7f7777adb0dcbd947ef1df6c866025da3c
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-15 18:52:59 -0500

    Add util for checking if token expired

---
+++ app/lib/utils.ts
@@ -0,0 +1,16 @@
+export function isTokenExpired() {
+  const token = localStorage.getItem("token");
+  const maxAgeMillis = 30 * 1000;
+
+  if (token) {
+    // Get the token date and current date
+    const tokenData = JSON.parse(token);
+    const tokenDateInMillis = tokenData.date.getTime();
+    const currentDate = new Date();
+
+    // Calculate difference
+    const differenceInMillis = currentDate.getTime() - tokenDateInMillis;
+
+    return differenceInMillis > maxAgeMillis;
+  }
+}
