292aa073f5a7e70a07c8a7958016643185f311a6
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-15 18:07:19 -0500

    Update authenticateUser method
    Parsed data from formData into json body. Set
    correct header. On success sets local storage
    entry and redirects. On fail sends error and
    clears input fields.

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,31 +1,54 @@
 "use client";
 
-import { useFormState, useFormStatus } from "react-dom";
-
-const authenticateUser = async (
-  prevState: string | undefined | void,
-  formData: FormData
-) => {
-  try {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/users/login`,
-      {
-        method: `POST`,
-        body: formData,
-      }
-    );
-    console.log(process.env.BACKEND_API_URL);
-    // const data = await response.json();
-  } catch (error) {
-    throw error;
-  }
-};
+import { useFormState } from "react-dom";
+import { useRouter } from "next/navigation";
+import { useRef } from "react";
 
 export default function LoginForm() {
+  const router = useRouter();
+
+  const ref = useRef<HTMLFormElement>(null);
+
+  const authenticateUser = async (
+    prevState: string | undefined | void,
+    formData: FormData
+  ) => {
+    try {
+      const body = {
+        username: formData.get("username"),
+        password: formData.get("password"),
+      };
+      console.log(JSON.stringify(body));
+
+      const response = await fetch(`http://localhost:8000/users/login`, {
+        method: `POST`,
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+
+      const data = await response.json();
+
+      if (!data.success) {
+        // Login unsuccessful so clear form and return error string
+        ref.current?.reset();
+        return "Incorrect login information.";
+      } else {
+        // Login successful so save token and redirect to /dashboard
+        localStorage.setItem("token", data.token);
+        router.push("/dashboard");
+      }
+    } catch (error) {
+      throw error;
+    }
+  };
+
   const [errorMessage, dispatch] = useFormState(authenticateUser, undefined);
 
   return (
-    <form action={dispatch}>
+    <form action={dispatch} ref={ref}>
       <div>
         <label htmlFor="username">Email:</label>
         <input
@@ -43,11 +66,22 @@ export default function LoginForm() {
           className="text-black"
           type="password"
           id="password"
-          name="pasword"
+          name="password"
           required
         />
       </div>
       <button type="submit">Log In</button>
+      <div
+        className="flex h-8 items-end space-x-1"
+        aria-live="polite"
+        aria-atomic="true"
+      >
+        {errorMessage !== undefined && (
+          <>
+            <p className="text-sm text-red-500">{errorMessage}</p>
+          </>
+        )}
+      </div>
     </form>
   );
 }
