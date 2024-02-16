85def014fba5d8683ec1cecd604676d5e771f39c
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-15 19:01:29 -0500

    Remove unneeded login function

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,22 +1,6 @@
 import { UserLogin, Post, Comment } from "@/app/lib/definitions";
 
-const api = process.env.BACKEND_API_URL;
-
-// Log in
-export async function getLoginToken(userLogin: UserLogin) {
-  // Send request to API then await and return response
-  try {
-    const jwtoken = await fetch(`${api}/users/login`, {
-      method: "POST",
-      body: JSON.stringify(userLogin),
-      cache: "no-store",
-    });
-    return jwtoken;
-  } catch (error) {
-    console.error("Database Error", error);
-    throw new Error("Failed to login.");
-  }
-}
+const api = "";
 
 // Get all the posts data
 export async function getPosts() {
